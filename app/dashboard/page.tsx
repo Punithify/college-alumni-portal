d53788fdbe7007e2dd7cf8925a2647ffda5958b6
Punithify <punithsp9@gmail.com>
2024-05-31 21:15:40 +0530

    deployment-check-2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,53 @@
-import React from "react"
-import { useRouter } from "next/navigation"
-import { getServerSession } from "next-auth"
-
+import { getStudentsData } from "@/lib/utils"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import SearchForm from "@/components/custom/SearchForm"
-import StudentCard from "@/components/custom/StudentCard"
+
+async function getData() {
+  const res = await getStudentsData()
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+
+  return res.json()
+}
 
 export default async function DashboardPage() {
-  const session = await getServerSession()
-  console.log(session?.user)
+  const data = await getData()
+  console.log(data)
 
   return (
-    <div>
-      <div className="flex justify-center items-center m-4">
+    <>
+      <div className="flex justify-center items-center mt-4">
         <SearchForm />
       </div>
-      <StudentCard />
-    </div>
+
+      <div className="mt-4 w-full flex justify-center">
+        <div className="flex flex-wrap justify-center space-x-4">
+          {data.map((item) => (
+            <div key={item.id} className="w-64">
+              <Card>
+                <CardHeader>
+                  <CardTitle>{item.username}</CardTitle>
+                  <CardDescription>{item.email}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>{item.department}</p>
+                </CardContent>
+                <CardFooter>
+                  <p>{item.graduation_year}</p>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
